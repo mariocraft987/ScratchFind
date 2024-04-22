2cbb4ae3b7434be2691fd03c63f30b22ad2eb1f8
Pixilized <166399565+PixilizedScratch@users.noreply.github.com>
2024-04-22 18:07:22 -0400

    Made toggle work (mabye, gonna have to commit to test it)

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 //Toggle light or dark mode
 function toggleTheme() {
-
+    let theme = getElementByTagName("html");
+    if (theme.className == "light") {
+        theme.className = "dark";
+    } else if (theme.className == "dark") {
+        theme.className = "light";
+    }
 }
