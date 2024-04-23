2ca2ae48346b77f10841781b97e5dbd6be514d33
Sihan <29PathiS@mbbc.qld.edu.au>
2024-04-23 13:14:54 +1000

    updated code

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
 import requests
 from pyscript import document
+
+
+url = document.querySelector("#url")
+display = document.querySelector("#display")
+display.innerText = "a"
+link = url.value
+
+def hello():
+    response = requests.get(link)
+    display.innerText = response.text
